f6730e83234aae5c148db621a810cc17448180bc
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2021-01-21 19:37:14 +0300

    наброски 2 3 4 функции

--- 9.js
+++ 9.js
@@ -13,3 +13,15 @@ function task1() {
 		}
 	console.log('В городе ', weather.city ,' сейчас ', weather.temp ,' по Цельсию. За окном ', weather.sky);
 }
+/// Задание 2 ///
+function task2() {
+	console.log("Задание №2");
+}
+/// Задание 3 ///
+function task3() {
+	console.log("Задание №3");
+}
+/// Задание 4 ///
+function task4() {
+	console.log("Задание №4");
+}
